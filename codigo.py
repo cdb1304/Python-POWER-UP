51abaf794a3fb71f7a7fea411c32fa33cc592d54
Caua Debattisti <cauabap@gmail.com>
2025-09-16 19:40:32 -0300

    giti init

--- codigo.py
+++ codigo.py
@@ -12,3 +12,11 @@ pyautogui.write("https://dlp.hashtagtreinamentos.com/python/intensivao/login")
 pyautogui.press ("enter")
 
 time.sleep(1)
+
+pyautogui.PAUSE = 0.5
+pyautogui.click(x=801, y=371)
+pyautogui.write("cauabap@gmail.com")
+pyautogui.click(x=717, y=471)
+pyautogui.write("Caua13bap")
+pyautogui.click(x=967, y=579)
+pyautogui.rightClick
